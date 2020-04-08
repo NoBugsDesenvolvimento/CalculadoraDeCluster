1338b6516cd7d5a1e12e4c0d80195c689bf7b73a
Renan905 <905renan@gmail.com>
2020-04-08 15:11:19 -0300

    WIP | Organização do arquivo

--- js/main.js
+++ js/main.js
@@ -1,90 +1,40 @@
 
+//  ------------------------- 
+// Modal
+function openNav() {
+    document.getElementById("modal-Meta").style.display = "block";
+}
+  
+function closeNav() {
+    document.getElementById("modal-Meta").style.display = "none";
+}
 
-$('#salvarNomeEj').click( function () {
-    if (($('#ejinput').val()) != '') {
-        $("#nomeej").text($('#ejinput').val().toUpperCase());
-        $("#tituloej").text($('#ejinput').val().toUpperCase());
-        
-        $("#ejinputContainer").css('visibility', 'hidden');
-        $('#containerNomeej').css('visibility', 'visible');
-
-        ClusterMeta();
-    }
+$(".modal-background").click( function (){
+    closeNav();
 });
 
-$('#ejinput').keyup(function() {
-
+$('#nomeEjInput').keyup(function() {
     if (($(this).val()) != '') {
         $("#tituloej").text($(this).val().toUpperCase());
     } else{
         $("#tituloej").text("SUA EMPRESA");
     }
-
-
 });
 
 
-
-$("#editarej").click(function () {
-    $('#containerNomeej').css('visibility', 'hidden');
-    $("#ejinputContainer").css('visibility', 'visible');
-    $("#ejinput").focus();
-});
-
-$(":radio").change(function() {
-    if ($("#inputMetaRadio").is(':checked')){
-        $("#inputMeta").prop('disabled', false);
-    } else{
-        $("#inputMeta").prop('disabled', true);
-    }
-});
-
-$("#salvarMeta").click( function() {
-    $("#cardEdicao").css("display", "none");
-    $("#footerCardSuaEJ").css("display", "inherit");
-    
-    ClusterMeta();
-});
+//  ----------------------------
+// Meta
 
 $("#editarMeta").click( function() {
     $("#footerCardSuaEJ").css("display", "none");
     $("#cardEdicao").css("display", "inherit");
 });
 
-
-
-
-// $("#dark-mode").click(function (){
-
-//     $("#dark-mode").css("visibility", "hidden");
-//     $("#light-mode").css("visibility", "visible");
-//     $("body").css("background-color", "hsl(204, 86%, 53%)");
-// });
-
-// $("#light-mode").click(function (){
-
-//     $("#light-mode").css("visibility", "hidden");
-//     $("#dark-mode").css("visibility", "visible");
-
-//     $("body").css("background-color", "hsl(204, 27%, 18%)");
-// });
-
-
-
-$("#share").jsSocials({
-    shares: ["twitter", "facebook", "linkedin", "whatsapp"]
+$("#salvarMeta").click( function() {
+    $("#cardEdicao").css("display", "none");
+    $("#footerCardSuaEJ").css("display", "inherit");
+    ClusterMeta();
 });
-
-
-/* Open */
-function openNav() {
-    document.getElementById("modal-Meta").style.display = "block";
-}
-  
-  /* Close */
-function closeNav() {
-    document.getElementById("modal-Meta").style.display = "none";
-}
 
 function SelecionarMeta(id){
     $("#1meta").removeClass('is-warning').addClass('is-primary');
@@ -94,6 +44,5 @@ function SelecionarMeta(id){
     $("#5meta").removeClass('is-warning').addClass('is-primary');
 
     $("#"+id).removeClass('is-primary').addClass('is-warning');
-
     localStorage.setItem("metaid", id);
 }
